f579783693d08a6295f94d2ee44f597d64ce396c
Mhia Mojica <mhiamojica@gmail.com>
2023-01-13 16:35:34 -0800

    fixing mistake

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -15,15 +15,6 @@ def index():
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
-    )
-
-@bp.route('/')
-def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
