ccc760c7019fbbac838cdd7055a987f472f19928
Mhia Mojica <mhiamojica@gmail.com>
2023-01-15 20:28:06 -0800

    index test for delete view

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -74,3 +74,12 @@ def test_create_update_validate(client, auth, path):
     response = client.post(path, data={'title': '', 'body': ''})
     assert b'Title is required.' in response.data
 
+def test_delete(client, auth, app):
+    auth.login()
+    response = client.post('/1/delete')
+    assert response.headers["Location"] == "/"
+
+    with app.app_context():
+        db = get_db()
+        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
+        assert post is None
